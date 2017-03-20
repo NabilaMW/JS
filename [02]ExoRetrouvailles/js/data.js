0e1810aa1ec3858eb2e801b52542115732f6ff00
NabilaMW <nabila.maidi@lepoles.com>
2017-03-20 17:05:54 +0100

    ajout2003

---
+++ [02]ExoRetrouvailles/js/data.js
@@ -0,0 +1,12 @@
+var data = {
+  'perso_0' : {
+    'name' : 'Chouwi',
+    'picture' : 'img/Kira.jpg',
+    'texte' : ['Je suis la justice! Je protège les innocents et ceux qui craignent le mal. Im celui qui deviendra le dieu d\'un monde nouveau que tout le monde veut !',' Pour gagner, vous devez attaquer','Écoutez ceci: Je ne suis pas seulement Kira, mais je suis aussi Dieu du nouveau monde!']
+  },
+  'perso_1' : {
+    'name' : 'Archibalde',
+    'picture' : 'img/Ryuk.jpg',
+    'texte' : ['Les humains sont intéressants.', 'Je ne vous ai pas choisi. Ne voyez-vous pas que ce soit tout juste un accident?','Pour moi ... Les pommes sont comme les cigarettes et l\'alcool pour les humains.']
+  }
+}
